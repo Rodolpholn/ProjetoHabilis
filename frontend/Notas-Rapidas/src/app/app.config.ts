2b7e701da3db46be7dae66654fd29d3e6bb952f7
Rodolpholn <rodolpho-crack@hotmail.com>
2025-08-15 23:44:14 -0300

    first commit frontend

---
+++ frontend/Notas-Rapidas/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // Adicione esta linha
+import { provideForms } from '@angular/forms'; // Adicione esta linha
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(), // Adicione esta linha no array de provedores
+    provideForms(), // Adicione esta linha no array de provedores
+  ],
+};
