1f477a32a6924e43898d2102efafae3ef2a8c705
Rodolpholn <rodolpho-crack@hotmail.com>
2025-08-16 23:18:19 -0300

    docs: Add detailed README for the job application project.

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,19 @@
 const express = require("express");
-const cors = require("cors"); // Importe o cors
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
-// Use o middleware cors para permitir requisições do frontend
 app.use(cors());
 
-// Middleware para analisar o corpo das requisições como JSON
 app.use(express.json());
 
-// Simulação de um banco de dados em memória
 const notesDB = [];
 let noteIdCounter = 1;
 
-// Endpoint POST para criar uma nova nota para um cliente
 app.post("/clients/:clientId/notes", (req, res) => {
   const clientId = req.params.clientId;
   const { text } = req.body;
 
-  // Validação básica: o texto da nota não pode ser vazio
   if (!text) {
     return res
       .status(400)
@@ -38,7 +33,6 @@ app.post("/clients/:clientId/notes", (req, res) => {
   res.status(201).json(newNote);
 });
 
-// Endpoint GET para listar todas as notas de um cliente
 app.get("/clients/:clientId/notes", (req, res) => {
   const clientId = req.params.clientId;
   const clientNotes = notesDB.filter((note) => note.clientId === clientId);
